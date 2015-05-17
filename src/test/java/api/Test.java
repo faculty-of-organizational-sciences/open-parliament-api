be2a2a9faa2a1ad3c98fbcaa0ca0440cd9a80f35
Branislav1993 <branislav.vidojevic@outlook.com>
2015-05-17 02:06:12 +0200

    Added basic hibernate.cfg.xml, HibernateUtil.java class, and Test class.

---
+++ src/test/java/api/Test.java
@@ -0,0 +1,24 @@
+package api;
+
+import java.util.List;
+import org.hibernate.Session;
+import rs.otvoreniparlament.api.domain.HibernateUtil;
+import rs.otvoreniparlament.api.domain.Profesija;
+
+public class Test {
+
+	public static void main(String[] args) {
+
+		Session session = HibernateUtil.getSessionFactory().openSession();
+		session.beginTransaction();
+
+		List<Profesija> list = session.createCriteria(Profesija.class).list();
+
+		for (int i = 0; i < list.size(); i++) {
+			System.out.println(list.get(i).toString());
+		}
+		
+		session.close();
+		HibernateUtil.shutdown();
+	}
+}
