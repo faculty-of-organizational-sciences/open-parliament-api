49e0f4c9c4c72e4b32b7f5ca9f4c283525e97194
Marija Milcic <milcic2812@fon.bg.ac.rs>
2015-05-23 19:20:25 +0200

    Mapped Poslanik class with error

--- src/test/java/api/Test.java
+++ src/test/java/api/Test.java
@@ -12,40 +12,46 @@ import rs.otvoreniparlament.api.domain.ImovinskaKarta;
 import rs.otvoreniparlament.api.domain.Lista;
 import rs.otvoreniparlament.api.domain.MedjunarodnaOrganizacija;
 import rs.otvoreniparlament.api.domain.Mesto;
+import rs.otvoreniparlament.api.domain.Poslanik;
 import rs.otvoreniparlament.api.domain.PolitickaOrganizacija;
 import rs.otvoreniparlament.api.domain.Profesija;
 import rs.otvoreniparlament.api.domain.WebPrezentacija;
 
-
 public class Test {
 
 	public static void main(String[] args) {
-						
+
 		Settings.getInstance();
-		
-		Session session = HibernateUtil.getInstance().getSessionFactory().openSession();
+
+		Session session = HibernateUtil.getInstance().getSessionFactory()
+				.openSession();
 		session.beginTransaction();
 
-//		List<Profesija> list = session.createCriteria(Profesija.class).list();
-//		List<Mesto> list = session.createCriteria(Mesto.class).list();
-//		List<Drzava> list = session.createCriteria(Drzava.class).list();
-//		List<GrupePrijateljstva> list = session.createCriteria(GrupePrijateljstva.class).list();
-//		List<Lista> list = session.createCriteria(Lista.class).list();
-//		List<ImovinskaKarta> list = session.createCriteria(ImovinskaKarta.class).list();
-//		List<MedjunarodnaOrganizacija> list = session.createCriteria(MedjunarodnaOrganizacija.class).list();
-//		List<WebPrezentacija> list = session.createCriteria(WebPrezentacija.class).list();
+		// List<Profesija> list =
+		// session.createCriteria(Profesija.class).list();
+		// List<Mesto> list = session.createCriteria(Mesto.class).list();
+		// List<Drzava> list = session.createCriteria(Drzava.class).list();
+		// List<GrupePrijateljstva> list =
+		// session.createCriteria(GrupePrijateljstva.class).list();
+		// List<Poslanik> list = session.createCriteria(Poslanik.class).list();
+		// List<Lista> list = session.createCriteria(Lista.class).list();
+		// List<ImovinskaKarta> list =
+		// session.createCriteria(ImovinskaKarta.class).list();
+		// List<MedjunarodnaOrganizacija> list =
+		// session.createCriteria(MedjunarodnaOrganizacija.class).list();
+		// List<WebPrezentacija> list =
+		// session.createCriteria(WebPrezentacija.class).list();
 		List<PolitickaOrganizacija> list = session.createCriteria(PolitickaOrganizacija.class).list();
-		
+
 		System.out.println("Velicina liste: " + list.size());
 
 		for (int i = 0; i < list.size(); i++) {
 			System.out.println(list.get(i).toString());
 		}
-		
+
 		System.out.println("Done!");
-		
+
 		session.close();
 		HibernateUtil.getInstance().shutdown();
 	}
 }
-
