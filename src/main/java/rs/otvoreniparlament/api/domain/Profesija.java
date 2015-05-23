a17b251e967f927753163f2a7cba29db61fa1910
Branislav1993 <branislav.vidojevic@outlook.com>
2015-05-23 19:07:31 +0200

    Created and mapped classes ImovinskaKarta,Lista,MedjunarodnaOrganizacija,PolitickaOrganizacija, WebPrezentacija.

--- src/main/java/rs/otvoreniparlament/api/domain/Profesija.java
+++ src/main/java/rs/otvoreniparlament/api/domain/Profesija.java
@@ -3,7 +3,6 @@ package rs.otvoreniparlament.api.domain;
 import javax.persistence.Basic;
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
--- src/test/java/api/Test.java
+++ src/test/java/api/Test.java
@@ -8,8 +8,13 @@ import rs.otvoreniparlament.api.config.Settings;
 import rs.otvoreniparlament.api.database.HibernateUtil;
 import rs.otvoreniparlament.api.domain.Drzava;
 import rs.otvoreniparlament.api.domain.GrupePrijateljstva;
+import rs.otvoreniparlament.api.domain.ImovinskaKarta;
+import rs.otvoreniparlament.api.domain.Lista;
+import rs.otvoreniparlament.api.domain.MedjunarodnaOrganizacija;
 import rs.otvoreniparlament.api.domain.Mesto;
+import rs.otvoreniparlament.api.domain.PolitickaOrganizacija;
 import rs.otvoreniparlament.api.domain.Profesija;
+import rs.otvoreniparlament.api.domain.WebPrezentacija;
 
 
 public class Test {
@@ -24,8 +29,12 @@ public class Test {
 //		List<Profesija> list = session.createCriteria(Profesija.class).list();
 //		List<Mesto> list = session.createCriteria(Mesto.class).list();
 //		List<Drzava> list = session.createCriteria(Drzava.class).list();
-		List<GrupePrijateljstva> list = session.createCriteria(GrupePrijateljstva.class).list();
-
+//		List<GrupePrijateljstva> list = session.createCriteria(GrupePrijateljstva.class).list();
+//		List<Lista> list = session.createCriteria(Lista.class).list();
+//		List<ImovinskaKarta> list = session.createCriteria(ImovinskaKarta.class).list();
+//		List<MedjunarodnaOrganizacija> list = session.createCriteria(MedjunarodnaOrganizacija.class).list();
+//		List<WebPrezentacija> list = session.createCriteria(WebPrezentacija.class).list();
+		List<PolitickaOrganizacija> list = session.createCriteria(PolitickaOrganizacija.class).list();
 		
 		System.out.println("Velicina liste: " + list.size());
 
