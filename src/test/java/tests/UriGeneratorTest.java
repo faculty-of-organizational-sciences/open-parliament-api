3ea4ea00a92797b9ef14445a377e068aade3010e
Branislav1993 <branislav.vidojevic@outlook.com>
2015-10-08 23:11:11 +0200

    Changed UriGeneratorTest to check if exception is thrown

--- src/test/java/tests/UriGeneratorTest.java
+++ src/test/java/tests/UriGeneratorTest.java
@@ -2,25 +2,11 @@ package tests;
 
 import static org.junit.Assert.assertEquals;
 
-
-
-
-
-
-
-
-
-import java.util.Arrays;
-import java.util.Collection;
-
-import javax.swing.plaf.synth.Region;
-
-import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.junit.runners.Parameterized;
-import org.junit.runners.Parameterized.Parameters;
+import org.junit.rules.ExpectedException;
 
+import rs.otvoreniparlament.api.config.Settings;
 import rs.otvoreniparlament.api.domain.Assembly;
 import rs.otvoreniparlament.api.domain.Country;
 import rs.otvoreniparlament.api.domain.FriendshipGroup;
@@ -28,62 +14,73 @@ import rs.otvoreniparlament.api.domain.Member;
 import rs.otvoreniparlament.api.domain.Party;
 import rs.otvoreniparlament.api.domain.PartyList;
 import rs.otvoreniparlament.api.domain.PlenarySession;
+import rs.otvoreniparlament.api.domain.Region;
 import rs.otvoreniparlament.api.domain.Speech;
 import rs.otvoreniparlament.api.domain.Town;
-import rs.otvoreniparlament.api.domain.meta.RestServiceName;
 import rs.otvoreniparlament.api.uri.UriGenerator;
+import rs.otvoreniparlament.api.uri.exception.MissingRestServiceNameAnnotationException;
 
-
-
-@RunWith(Parameterized.class)
 public class UriGeneratorTest {
+	
+	String prefix = Settings.getInstance().config.uriGenerator.uriPrefix;
 
-	private String result;
-	static Object o;
-	static Member m;
-	static PlenarySession ps;
-	static Speech s;
-	static Party p;
-	static Assembly a;
-	static Country c;
-	static Town t;
-	static FriendshipGroup fg;
-	static PartyList pl;
-	static rs.otvoreniparlament.api.domain.Region r;
+	@Rule
+	public ExpectedException expected = ExpectedException.none();
+
+	@Test
+	public void testFriendshipGroupUri() {
+		expected.expect(MissingRestServiceNameAnnotationException.class);
+		UriGenerator.generate(new FriendshipGroup(), 155);
+	}
+
+	@Test
+	public void testAssemblyUri() {
+		expected.expect(MissingRestServiceNameAnnotationException.class);
+		UriGenerator.generate(new Assembly(), 155);
+	}
+
+	@Test
+	public void testCountryUri() {
+		expected.expect(MissingRestServiceNameAnnotationException.class);
+		UriGenerator.generate(new Country(), 155);
+	}
 	
 
-	public UriGeneratorTest(Object o, String result) {
-		super();
-		this.o= o;
-		this.result = result;
-	}
-	 @Before public void initialize() {
-	       m = new Member();
-	       ps = new PlenarySession();
-	       p  = new Party();
-	       s = new Speech();
-	       a = new Assembly();
-	       c = new Country();
-	       t = new Town();
-	       fg = new FriendshipGroup();
-	       pl = new PartyList();
-	       r = new rs.otvoreniparlament.api.domain.Region();
-	    }
-	@Parameters
-	public static Collection<Object[]> data() {
-		return Arrays.asList(new Object[][] { {"Passed object's class does not have declared RestServiceName annotation", UriGenerator.generate(fg, 895) },
-												{"Passed object's class does not have declared RestServiceName annotation", UriGenerator.generate(pl, 895) },
-													{"Passed object's class does not have declared RestServiceName annotation", UriGenerator.generate(r, 895) },
-														{"Passed object's class does not have declared RestServiceName annotation", UriGenerator.generate(a, 895) },
-															{"Passed object's class does not have declared RestServiceName annotation", UriGenerator.generate(c, 895) },
-																{"Passed object's class does not have declared RestServiceName annotation", UriGenerator.generate(t, 895) },
-																	{"http://api.otvoreniparlament.rs/v1/members/179", UriGenerator.generate(m, 179) },
-																		{"http://api.otvoreniparlament.rs/v1/sessions/889", UriGenerator.generate(ps, 889)},
-																			{"http://api.otvoreniparlament.rs/v1/speeches/179", UriGenerator.generate(s, 28808) },
-																				{"http://api.otvoreniparlament.rs/v1/parties/420", UriGenerator.generate(p, 420)}});
-	}
 	@Test
-	public void testFormat() {
-		assertEquals(o, result);
+	public void testPartyListUri() {
+		expected.expect(MissingRestServiceNameAnnotationException.class);
+		UriGenerator.generate(new PartyList(), 155);
+	}
+	
+	@Test
+	public void testRegionUri() {
+		expected.expect(MissingRestServiceNameAnnotationException.class);
+		UriGenerator.generate(new Region(), 155);
+	}
+	
+	@Test
+	public void testTownUri() {
+		expected.expect(MissingRestServiceNameAnnotationException.class);
+		UriGenerator.generate(new Town(), 155);
+	}
+	
+	@Test
+	public void testMemberUri(){
+		assertEquals(UriGenerator.generate(new Member(), 155), prefix + "members/155");
+	}
+	
+	@Test
+	public void testPartyUri(){
+		assertEquals(UriGenerator.generate(new Party(), 155), prefix + "parties/155");
+	}
+	
+	@Test
+	public void testSessionUri(){
+		assertEquals(UriGenerator.generate(new PlenarySession(), 155), prefix + "sessions/155");
+	}
+	
+	@Test
+	public void testSpeechUri(){
+		assertEquals(UriGenerator.generate(new Speech(), 155), prefix + "speeches/155");
 	}
 }
