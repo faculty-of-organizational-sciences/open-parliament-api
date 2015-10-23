37f37d738321e0495b3f1929db9fcb00eef5f410
Branislav1993 <branislav.vidojevic@outlook.com>
2015-10-23 20:09:07 +0200

    adedd agenda into speech json parser as an array of string

--- src/main/java/rs/otvoreniparlament/api/rest/parsers/SpeechJsonParser.java
+++ src/main/java/rs/otvoreniparlament/api/rest/parsers/SpeechJsonParser.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
+import com.google.gson.JsonPrimitive;
 
 import rs.otvoreniparlament.api.domain.Member;
 import rs.otvoreniparlament.api.domain.PlenarySession;
@@ -38,43 +39,55 @@ public class SpeechJsonParser {
 
 			jsonSpeech.addProperty("id", s.getId());
 
-			jsonSpeech.addProperty("text", s.getText().replaceAll("\\<.*?>",""));
+			jsonSpeech.addProperty("text", s.getText().replaceAll("\\<.*?>", ""));
 
 			jsonSpeech.addProperty("sessionDate", DateFormatter.format(s.getSessionDate()));
-			
+
+			if (s.getPlenarySession() != null) {
+
+				String[] agenda = s.getPlenarySession().getAgenda().split("\\s\\(\\d\\d\\d\\d\\)\\s");
+
+				JsonArray agendaArray = new JsonArray();
+
+				for (String a : agenda) {
+					agendaArray.add(new JsonPrimitive(a));
+				}
+
+				jsonSpeech.add("sessionAgenda", agendaArray);
+			}
+
 			JsonObject creatorJson = new JsonObject();
-			
-			if(s.getMember() != null){
-				
+
+			if (s.getMember() != null) {
+
 				Member m = s.getMember();
-				
+
 				creatorJson.addProperty("firstName", m.getName());
-				
+
 				creatorJson.addProperty("lastName", m.getLastName());
-				
-				JsonObject creatorMeta = new JsonObject();				
+
+				JsonObject creatorMeta = new JsonObject();
 				creatorMeta.addProperty("href", UriGenerator.generate(m, m.getId()));
-				
+
 				creatorJson.add("meta", creatorMeta);
-				
+
 			} else {
 				creatorJson.addProperty("error", "There is no Member with the given ID.");
 			}
 			jsonSpeech.add("creator", creatorJson);
-			
-			
+
 			JsonObject plenarySession = new JsonObject();
 
 			if (s.getPlenarySession() != null) {
 
 				PlenarySession ps = s.getPlenarySession();
-				
+
 				JsonObject plenarySessionMeta = new JsonObject();
-				
+
 				plenarySessionMeta.addProperty("href", UriGenerator.generate(ps, ps.getId()));
 
 				plenarySession.add("meta", plenarySessionMeta);
-				
+
 			} else {
 				plenarySession.addProperty("error", "There is no plenary Session with the given ID.");
 			}
