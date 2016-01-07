d32c61d75074af11be8f9dd29eae58c1bb21cd76
marij <marij@192.168.1.3>
2016-01-07 21:02:52 +0100

    closed #30

---
+++ src/main/java/rs/otvoreniparlament/indexing/StartIndexing.java
@@ -0,0 +1,24 @@
+package rs.otvoreniparlament.indexing;
+
+public class StartIndexing {
+
+	public static void main(String[] args) {
+		IndexingMembers im = new IndexingMembers();
+		im.deleteMembers();
+		System.out.println("Deleted... Indexing started for members!");
+		im.indexMembers();
+		IndexingSpeeches is = new IndexingSpeeches();
+		is.deleteSpeeches();
+		System.out.println("Deleted... Indexing started for speeches!");
+		is.indexSpeeches();
+		IndexingParties ip = new IndexingParties();
+		ip.deleteParties();
+		System.out.println("Deleted... Indexing started for parties!");
+		ip.indexParties();
+		IndexingPlearySessions ips = new IndexingPlearySessions();
+		ips.deleteParties();
+		System.out.println("Deleted... Indexing started for sessions!");
+		ips.indexPlenarySessions();
+		System.out.println("THE END!");
+	}
+}
