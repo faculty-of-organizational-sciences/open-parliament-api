a81c64ac4cdb54862fa375af7d4b45d0984fca3d
marij <marij@DESKTOP-DUFKO2D>
2016-02-07 13:47:58 +0100

    Changed class for search, added parameters for queries, indexes and types

--- src/main/java/rs/otvoreniparlament/api/index/ElasticSearchService.java
+++ src/main/java/rs/otvoreniparlament/api/index/ElasticSearchService.java
@@ -1,15 +1,12 @@
 package rs.otvoreniparlament.api.index;
 
-import java.util.Map;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.elasticsearch.action.search.SearchResponse;
 import org.elasticsearch.action.search.SearchType;
-import org.elasticsearch.common.unit.TimeValue;
 import org.elasticsearch.index.query.QueryBuilder;
 import org.elasticsearch.index.query.QueryBuilders;
-import org.elasticsearch.search.SearchHit;
 
 
 public class ElasticSearchService {
@@ -20,44 +17,38 @@ public class ElasticSearchService {
 	// Search
 	public  SearchResponse searchQuery(String index, String name, String query) {
 				
-		QueryBuilder qb = QueryBuilders.queryStringQuery(query);
+		QueryBuilder qb = QueryBuilders.queryStringQuery(query + "*");
+		
 		searchResponse = ElasticClient.getInstance().getClient().prepareSearch(index)
 				.setTypes(name).setSearchType(SearchType.DFS_QUERY_THEN_FETCH)
 				.setQuery(qb) // Query
 				.setFrom(0).setSize(60).setExplain(true).execute().actionGet();
-		logger.info("test... >>>>" + searchResponse );
-		
-		SearchHit[] hits = searchResponse.getHits().getHits();
-        System.out.println("Current results: " + hits.length);
-        for (SearchHit hit : hits) {
-            Map<String,Object> result = hit.getSource();   
-            System.out.println(result);
-        }
 	
 		return searchResponse;
 	}
 	
-	public SearchResponse test (String query){
-		QueryBuilder qb =QueryBuilders.queryStringQuery(query);
-
-		SearchResponse scrollResp = ElasticClient.getInstance().getClient().prepareSearch("datasearch")
-		        .setSearchType(SearchType.SCAN)
-		        .setScroll(new TimeValue(60000))
-		        .setQuery(qb)
-		        .setSize(100).execute().actionGet(); //100 hits per shard will be returned for each scroll
-		//Scroll until no hits are returned
-		while (true) {
-
-		    for (SearchHit hit : scrollResp.getHits().getHits()) {
-		    	 Map<String,Object> result = hit.getSource();   
-		            System.out.println(result);
-		    }
-		    scrollResp = ElasticClient.getInstance().getClient().prepareSearchScroll(scrollResp.getScrollId()).setScroll(new TimeValue(600000)).execute().actionGet();
-		    //Break condition: No hits are returned
-		    if (scrollResp.getHits().getHits().length == 0) {
-		        break; }}
-		    return scrollResp;
-	}
+//	public SearchResponse test (String query){
+//		QueryBuilder qb =QueryBuilders.queryStringQuery(query);
+//
+//		SearchResponse scrollResp = ElasticClient.getInstance().getClient().prepareSearch("datasearch")
+//		        .setSearchType(SearchType.SCAN)
+//		        .setScroll(new TimeValue(60000))
+//		        .setQuery(qb)
+//		        .setSize(100).execute().actionGet(); //100 hits per shard will be returned for each scroll
+//		//Scroll until no hits are returned
+//		while (true) {
+//
+//		    for (SearchHit hit : scrollResp.getHits().getHits()) {
+//		    	 Map<String,Object> result = hit.getSource();   
+//		            System.out.println(result);
+//
+//		    }
+//		    scrollResp = ElasticClient.getInstance().getClient().prepareSearchScroll(scrollResp.getScrollId()).setScroll(new TimeValue(600000)).execute().actionGet();
+//		    //Break condition: No hits are returned
+//		    if (scrollResp.getHits().getHits().length == 0) {
+//		        break; }}
+//		    return scrollResp;
+//	}
 
 	public SearchResponse searchResponse;
 
@@ -67,12 +58,6 @@ public class ElasticSearchService {
 
 	public void setSearchResponse(SearchResponse searchResponse) {
 		this.searchResponse = searchResponse;
-	}
-
-	public static void main(String[] args) {
-		ElasticSearchService elastic = new ElasticSearchService();
-		elastic.searchQuery("","member", "aleksandar");
-
 	}
 
 }
